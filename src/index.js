053734d35e1b692c83a3ec9a1dccf97095020960
Miriama Svitkova <miriama.svitkova@reaktor.com>
2024-04-29 15:25:35 +0200

    Start work on error messages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 import { intervalToDuration, parse } from "date-fns";
 
+function isRequired(value) {
+  if (!value) {
+    return "This field is required";
+  } else {
+    return "";
+  }
+}
+
 function getBirthdayDate(e) {
   e.preventDefault();
+  const errMsgs = document.querySelectorAll(".err-msg");
+  Array.from(errMsgs).map((el) => (el.innerText = ""));
+
   const dayInputEl = document.getElementById("day");
   const monthInputEl = document.getElementById("month");
   const yearInputEl = document.getElementById("year");
 
+  const dayErrorMessage = isRequired(dayInputEl.value);
+  const monthErrorMessage = isRequired(monthInputEl.value);
+  const yearErrorMessage = isRequired(yearInputEl.value);
+
+  if (dayErrorMessage) {
+    const errMessageEl = document.getElementById("err-msg-day");
+    errMessageEl.innerText = dayErrorMessage;
+    return;
+  }
+
+  if (monthErrorMessage) {
+    const errMessageEl = document.getElementById("err-msg-month");
+    errMessageEl.innerText = monthErrorMessage;
+    return;
+  }
+
+  if (yearErrorMessage) {
+    const errMessageEl = document.getElementById("err-msg-year");
+    errMessageEl.innerText = yearErrorMessage;
+    return;
+  }
+
   const birthdayValue = `${dayInputEl.value}/${monthInputEl.value}/${yearInputEl.value}`;
-  //dd/mm/yyyy
+
   calculateAge(birthdayValue);
 }
 
@@ -17,17 +50,24 @@ function calculateAge(dob) {
 
   const interval = intervalToDuration({ start: birthdayDate, end: dateToday });
 
-  const ageYears = interval.years || 0;
-  const ageMonths = interval.months || 0;
-  const ageDays = interval.days || 0;
+  const age = {
+    years: interval.years || 0,
+    months: interval.months || 0,
+    days: interval.days || 0,
+  };
+  renderAge(age);
+}
+
+function renderAge(age) {
+  const { years, months, days } = age;
 
   const ageYearsEl = document.getElementById("age-years");
   const ageMonthsEl = document.getElementById("age-months");
   const ageDaysEl = document.getElementById("age-days");
 
-  ageYearsEl.innerHTML = ageYears;
-  ageMonthsEl.innerHTML = ageMonths;
-  ageDaysEl.innerHTML = ageDays;
+  ageYearsEl.innerHTML = years;
+  ageMonthsEl.innerHTML = months;
+  ageDaysEl.innerHTML = days;
 }
 
 const birthdayForm = document.getElementById("birthday-form");
